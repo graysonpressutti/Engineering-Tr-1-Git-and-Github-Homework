c0d099ba25850b43a57b64ccb4f3ac00aa6d0d7d
Grayson Pressutti <grayson.pressutti@gmail.com>
2026-07-06 20:39:19 -0700

    Fixed previous commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import scenery from './scenery.jpeg'
+import './App.css'
 
 function App() {
   return (
     <div className="container">
-      <h1>Yosemite National Park</h1>
+      <h1 className="title">Yosemite National Park</h1>
       <img src={scenery} className="scenery-img" alt="A beautiful scenery" />
       <p className="description">
         A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail.
