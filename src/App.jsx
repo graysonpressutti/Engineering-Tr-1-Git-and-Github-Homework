f5ad6ad27205d29cf3260629acff523b8d848f7c
Grayson Pressutti <grayson.pressutti@gmail.com>
2026-07-06 21:01:34 -0700

    Altered the format of the paragraph code in app.jsx (no fix)

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
       <div className="container">
         <h1 className="title">Yosemite National Park</h1>
         <img src={scenery} className="scenery-img" alt="A beautiful scenery" />
-        <p className="description">
-         A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail.
-        </p>
+        <p className="description">"A picture of Yosemite National Park's Half Dome, halfway through the 4 Mile Trail."</p>
       </div>
     </>
   )
